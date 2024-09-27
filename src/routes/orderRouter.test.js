52bf3f29c497ec8801bb2c2c67503943b721f635
Ldach <lukedach@icloud.com>
2024-09-27 00:55:34 -0600

    Put item test (90.96% line coverage)

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -5,7 +5,6 @@ const request = require('supertest');
 const app = require('../service');
 
 ////////////////////////////////////
-/*
 const { Role, DB } = require('../database/database.js');
 function randomName() {
   return Math.random().toString(36).substring(2, 12);
@@ -21,13 +20,14 @@ async function createAdminUser() {
   user.password = 'toomanysecrets';
   return user;
 }
-  */
+
 //////////////////////////////////////////////////
 
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 let testUserUser;
+let admin;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
@@ -42,6 +42,23 @@ test('get menu', async () => {
 });
 
 
+test('put item', async () => {
+    admin = await createAdminUser();
+
+    const loginRes = await request(app).put('/api/auth').send(admin);
+    const tempAuth = await loginRes.body.token;
+
+    let newItem =  {title:"temp", description: "No topping, no sauce, just carbs", image:"pizza9.png", price: 0.0001 };
+    newItem.description = randomName();
+    newItem.title = randomName();
+    newItem.image = randomName();
+    newItem.price = 0.214223;
+     
+    const franchiseRes = await request(app).put('/api/order/menu').set('Content-Type', 'application/json').set('Authorization', `Bearer ${tempAuth}`).send(newItem);
+
+    expect(franchiseRes.status).toBe(200);
+  });
+
 test('get orders', async () => {
     const logoutRes = await request(app).get('/api/order').set('Authorization', `Bearer ${testUserAuthToken}`).send(testUser);
     expect(logoutRes.status).toBe(200);
@@ -50,6 +67,7 @@ test('get orders', async () => {
 
   test('create order', async () => {
     const testOrder = {franchiseId: 1, storeId: 1, items: [{ menuId: 1, description: "Veggie", price: 0.05 }]};
+    testOrder.items.description = randomName();
     const logoutRes = await request(app).post('/api/order').set('Content-Type', 'application/json').set('Authorization', `Bearer ${testUserAuthToken}`).send(testOrder);
     expect(logoutRes.status).toBe(200);
   });
