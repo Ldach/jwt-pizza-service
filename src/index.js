0191a23fdd9bee7d8b6b036af3a92144cd7e779f
Ldach <lukedach@icloud.com>
2024-12-12 16:28:42 -0700

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-const express = require('express');
 const app = require('./service.js');
-const { trackRequest } = require('./metrics.js');
+const { sendMetricsPeriodically } = require('./metrics.js');
 
 const port = process.argv[2] || 3000;
 
-app.use(trackRequest);
-
-sendMetricsPeriodically(5 * 60 * 1000); 
+sendMetricsPeriodically(10000);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
