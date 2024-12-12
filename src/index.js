ea38a1e5822311acc9243c428277c919b718da4e
Ldach <lukedach@icloud.com>
2024-12-12 16:25:37 -0700

    Index and Metrics http tracking fixxes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const express = require('express'); 
-const metrics = require('./metrics.js'); 
-
-const app = express();
-
-app.use(metrics.requestTracker);
+const express = require('express');
+const app = require('./service.js');
+const { trackRequest } = require('./metrics.js');
 
 const port = process.argv[2] || 3000;
+
+app.use(trackRequest);
+
+sendMetricsPeriodically(5 * 60 * 1000); 
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
--- src/metrics.js
+++ src/metrics.js
@@ -1,74 +1,44 @@
-const config = require('./config.js');
-class Metrics {
-  constructor() {
-    this.totalRequests = 0;
-    this.totalGetRequests = 0;
-    this.totalPostRequests = 0;
-    this.totalDeleteRequests = 0;
+const MetricBuilder = require('metric-builder');
+const config = require('../config.js'); 
 
-    const timer = setInterval(() => {
-      this.sendMetricToGrafana('request', 'all', 'total', this.totalRequests);
-      this.sendMetricToGrafana('request', 'get', 'total', this.totalGetRequests);
-      this.sendMetricToGrafana('request', 'post', 'total', this.totalPostRequests);
-      this.sendMetricToGrafana('request', 'delete', 'total', this.totalDeleteRequests);
-    }, 10000);
-    timer.unref();
+let requestMetrics = {
+  GET: 0,
+  POST: 0,
+  PUT: 0,
+  DELETE: 0,
+  total_requests: 0,
+};
+
+function trackRequest(req, res, next) {
+  const method = req.method;
+  if (method === 'GET' || method === 'POST' || method === 'PUT' || method === 'DELETE') {
+    requestMetrics[method]++;
   }
-
-  incrementRequests() {
-    this.totalRequests++;
-  }
-
-  incrementGetRequests() {
-    this.totalGetRequests++;
-  }
-
-  incrementPostRequests() {
-    this.totalPostRequests++;
-  }
-
-  incrementDeleteRequests() {
-    this.totalDeleteRequests++;
-  }
-
-  sendMetricToGrafana(metricPrefix, httpMethod, metricName, metricValue) {
-    const metric = `${metricPrefix},source=${config.source},method=${httpMethod} ${metricName}=${metricValue}`;
-
-    fetch(`${config.url}`, {
-      method: 'post',
-      body: metric,
-      headers: { Authorization: `Bearer ${config.userId}:${config.apiKey}` },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          console.error('Failed to push metrics data to Grafana');
-        } else {
-          console.log(`Pushed ${metric}`);
-        }
-      })
-      .catch((error) => {
-        console.error('Error pushing metrics:', error);
-      });
-  }
-  
-  requestTracker = (req, res, next) => {
-    this.incrementRequests();
-    switch (req.method) {
-      case 'GET':
-        this.incrementGetRequests();
-        break;
-      case 'POST':
-        this.incrementPostRequests();
-        break;
-      case 'DELETE':
-        this.incrementDeleteRequests();
-        break;
-      default:
-        break;
-    }
-    next();
-  };
+  requestMetrics.total_requests++;
+  next();
 }
 
-const metrics = new Metrics();
-module.exports = metrics;
+function sendMetricsToGrafana(metrics) {
+  console.log('Sending metrics to Grafana:', metrics);
+}
+
+function sendMetricsPeriodically(period) {
+  setInterval(() => {
+    try {
+      const buf = new MetricBuilder();
+      
+      buf.addMetric('http_requests', requestMetrics.GET, 'method=GET');
+      buf.addMetric('http_requests', requestMetrics.POST, 'method=POST');
+      buf.addMetric('http_requests', requestMetrics.PUT, 'method=PUT');
+      buf.addMetric('http_requests', requestMetrics.DELETE, 'method=DELETE');
+      buf.addMetric('http_requests', requestMetrics.total_requests, 'method=total_requests');
+
+      const metrics = buf.toString('\n');
+      sendMetricsToGrafana(metrics);
+    } catch (error) {
+      console.log('Error sending metrics', error);
+    }
+  }, period);
+}
+
+module.exports = { trackRequest, sendMetricsPeriodically };
